6c44cefe55b5389371a05040d77f218e27d94a47
Radovan Bosy <radovan@bosy.sk>
2025-07-13 16:03:09 +0200

    Dorobeny vypis zobrazenia produktov + paginator

--- src/views/produktyView.vue
+++ src/views/produktyView.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { useGeneralDataStore } from '@/stores/generalDataStore';
-import { type Uzivatel } from '@/types/types';
+import ProduktyPaginator from './produktyView/produktyPaginator.vue';
 import {ref, onBeforeMount,computed} from 'vue'
 const generalDataStore=useGeneralDataStore()
 import ProduktBox from './produktyView/produktBox.vue';
 import { produkty } from '@/use/produkty';
 
 const page=ref<number>(1)
-const perPage=5;
+const perPage=10;
 
 const maxPages = computed(() => {
   return Math.ceil(generalDataStore.produkty.length / perPage);
@@ -24,25 +24,19 @@ onBeforeMount(()=>{
     produkty()
   }
 })
-
-
-
-
-
-
 </script>
 <template>
 <div class="p-4 sm:ml-64 ">
   <template v-if="generalDataStore.produkty.length==0">
     Nenašli sa žiadne produkty
   </template>
+  <ProduktyPaginator v-if="generalDataStore.produkty.length>0" :page="page" :maxPages="maxPages" @page-change="(e)=>page=e"></ProduktyPaginator>
   <div v-if="generalDataStore.produkty.length>0" class="grid w-full grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 my-4">
     <template v-for="produkt in aktualneProduktyNaStranke" :key="`produkt-${produkt.id}`">
       <ProduktBox :produkt="produkt"></ProduktBox>
     </template>
   </div>
-
-
+  <ProduktyPaginator v-if="generalDataStore.produkty.length>0" :page="page" :maxPages="maxPages" @page-change="(e)=>page=e"></ProduktyPaginator>
 </div>
 
 </template>
---
+++ src/views/produktyView/produktyPaginator.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { useGeneralDataStore } from '@/stores/generalDataStore';
+
+const generalDataStore=useGeneralDataStore()
+const props=defineProps<{page:number,maxPages:number}>()
+
+const emits=defineEmits<{
+  (event:'page-change',num:number):number
+}>()
+
+
+</script>
+<template>
+  <div class="flex justify-center space-x-1 mx-2 items-stretch  select-none my-2">
+    <div class="bg-white text-blue-900 rounded-md mr-4 text-lg p-1.5 items-center user-select-none" :title="`Stánka ${props.page} z ${maxPages}`">
+      {{ props.page }}/{{
+        props.maxPages
+      }}
+    </div>
+    <template v-if="props.page > 1">
+      <button
+        @click.prevent="emits('page-change',props.page-1)"
+        class="bg-white text-blue-900 hover:text-white hover:bg-blue-900 rounded-md" title="Predchádzajúca stránka">
+      <svg class="w-10 h-10"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="8" y1="12" x2="16" y2="12" />  <line x1="8" y1="12" x2="12" y2="16" />  <line x1="8" y1="12" x2="12" y2="8" /></svg>
+      </button>
+    </template>
+    <template v-else>
+      <button disabled class="bg-gray-400 text-gray-800 rounded-md disabled:opacity-20">
+        <svg class="w-10 h-10 text-gray-200"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="8" y1="12" x2="16" y2="12" />  <line x1="8" y1="12" x2="12" y2="16" />  <line x1="8" y1="12" x2="12" y2="8" /></svg>
+      </button>
+    </template>
+    <template
+      v-if="props.maxPages > props.page"
+    >
+      <button
+        @click.prevent="emits('page-change',props.page+1)"
+        class="bg-white text-blue-900 hover:text-white hover:bg-blue-900 rounded-md" title="Ďalšia stránka"
+      >
+      <svg class="w-10 h-10"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="16" y1="12" x2="8" y2="12" />  <line x1="16" y1="12" x2="12" y2="16" />  <line x1="16" y1="12" x2="12" y2="8" /></svg>
+      </button>
+    </template>
+    <template v-else>
+      <button disabled class="bg-gray-400 text-gray-800 rounded-md disabled:opacity-20">
+        <svg class="w-10 h-10 text-gray-200"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="16" y1="12" x2="8" y2="12" />  <line x1="16" y1="12" x2="12" y2="16" />  <line x1="16" y1="12" x2="12" y2="8" /></svg>
+      </button>
+    </template>
+  </div>
+
+</template>
